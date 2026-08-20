ea5722d6f378bcb493793b968495e65773c0f323
puskarwagle <puskarwagle17@gmail..com>
2026-08-20 20:18:31 +0545

    chore: remove legacy python, sqlite, and run.sh setup

--- src/web-svelte/src/routes/api/symbols/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit';
-import { getSymbols } from '$lib/dataLoader';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async () => {
-	const symbols = await getSymbols();
-	return json({ symbols });
-};
